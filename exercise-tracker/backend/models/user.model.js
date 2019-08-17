1c6801cb6826ae6a61a183623f729776a8675162
gerosantacruz <geronimo.santacruz2017@gmail.com>
2019-08-17 18:44:35 -0300

    bug fixed adn env ignored

--- exercise-tracker/backend/models/user.model.js
+++ exercise-tracker/backend/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    uername:{
+    username:{
         type:String,
         required: true,
         unique:true,
