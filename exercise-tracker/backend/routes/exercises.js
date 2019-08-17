559402f10c5ab8aa94ea77fe07c43ba6e6fdf5bd
gerosantacruz <geronimo.santacruz2017@gmail.com>
2019-08-17 20:17:01 -0300

    FrontEnd
    The backend is finish now is time to start with the front end

--- exercise-tracker/backend/routes/exercises.js
+++ exercise-tracker/backend/routes/exercises.js
@@ -25,4 +25,32 @@ router.route('/add').post((req,res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req,res) => {
+    Exercise.findById(req.params.id)
+        .then( exercises => res.json(exercises))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+router.route('/:id').delete((req,res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req,res) => {
+    Exercise.findById(req.params.id)
+        .then(exercises => {
+            exercises.username = req.body.username;
+            exercises.description = req.body.description;
+            exercises.duration = Number(req.body.duration);
+            exercises.date = Date.parse(req.body.date);
+
+            exercises.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports =  router;
